77a94efb03c2adf71c8b2f068c483ce184358120
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2025-02-07 17:19:47 +0000

    pagination done

--- estate_app/utils.py
+++ estate_app/utils.py
@@ -12,17 +12,23 @@ def sendmail(doc, recipients, msg, title, attachment=None):
         email_args["attachments"] = attachment
     frappe.enqueue(method=frappe.sendmail, queue='short', timeout=300, **email_args)
 
-def paginate(doctype, page=0):
+def paginate(doctype, page=0,conditions="",paginate_by=6):
     prev, next = 0, 0
     conditions = ""
     query = f"""SELECT creation, name, property_name, status, address, grand_total, image 
                 FROM `tab{doctype}` {conditions} ORDER BY creation DESC"""
     
-    if(page):
-        page = int(page)
-        properties = frappe.db.sql(query + f" LIMIT {(page*4)-4}, 4", as_dict=True)
-        next_set = frappe.db.sql(query + f" LIMIT {(page*4)}, 4", as_dict=True)  # to find if new set values exist or page next exists
-        if(next_set):
+    if page:
+        try:
+            page = int(page)
+        except ValueError:
+            #fall back
+            page = 1
+        properties = frappe.db.sql(query + f" LIMIT {(page*paginate_by)-paginate_by}, {paginate_by}", as_dict=True)
+        next_set = frappe.db.sql(query + f" LIMIT {(page*paginate_by)}, {paginate_by}", as_dict=True)  # to find if new set values exist or page next exists
+        # print(f"Next set: {page},{properties},{next_set}")
+        if next_set:
+            
             prev, next = page-1, page+1
         else:
             prev, next = page-1, 0
--- estate_app/www/property/index.py
+++ estate_app/www/property/index.py
@@ -1,15 +1,27 @@
 import frappe
 from estate_app.utils import paginate
 
+no_cache = 1 # used to stop the caching of the page in pagination
+
+
 def get_context(context):
     # Check if the 'page' parameter is passed in the URL
     page = frappe.form_dict.page
+    # check if search request
+    conditions=" "
+    type,status,city= frappe.form_dict.type,frappe.form_dict.status,frappe.form_dict.city
+    print(f"\n\n\n{type} {status} {city}\n\n\n")
     print(f"{frappe.form_dict}")
-    print(f"Received page: {page}")  # Debug print to check if the page is correctly received
+    print(f"\n\n\nReceived page: {page}\n\n\n")  # Debug print to check if the page is correctly received
 
-    paginate_context = paginate("Property", page)
+    if(type and status and city):
+        conditions= f"""AND property_type='{type}' AND status='{status}' AND city='{city}'"""
+
+    
+    paginate_context = paginate(doctype= "Property", page=page, conditions=conditions) # passing conditions to the paginate function
     print("Pagination function call is finished")  # Debug print to track function execution
-
+    context.cities = frappe.db.sql("SELECT name FROM `tabCity`", as_dict=True)
+    context.types = frappe.db.sql("SELECT name FROM `tabProperty Type`", as_dict=True)
     context.properties = paginate_context.get('properties')
     context.prev = paginate_context.get('prev')
     context.next = paginate_context.get('next') 
