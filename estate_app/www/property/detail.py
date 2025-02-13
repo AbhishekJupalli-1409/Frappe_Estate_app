32d2c06b26dd6e42144b7f0dd660db4cb1ab469e
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2025-02-13 11:31:02 +0000

    fixed client mail by github commit

--- estate_app/www/property/detail.py
+++ estate_app/www/property/detail.py
@@ -4,9 +4,10 @@ import frappe
 def get_context(context):
   
     try:
-        
+        print(f"\n\n{frappe.form_dict}\n\n")
         docname = frappe.form_dict.docname
         context.property = frappe.get_doc('Property',frappe.form_dict.docname )
+        print(f"\n\n{context.property}\n\n")
         context.agent = frappe.get_doc('Agent',context.property.agent)
         related_properties = frappe.db.sql(f"""SELECT creation,name,property_name,status,address,grand_total,image FROM `tabProperty` WHERE property_type='{context.property.property_type}' AND name!='{context.property.name}' ORDER BY creation DESC LIMIT 3; """, as_dict=True)
         context.related_properties = related_properties
