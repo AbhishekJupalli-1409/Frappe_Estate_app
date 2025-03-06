f719a045b924bcb0d1c5527bf5306ef8f048dcc2
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2025-03-06 12:22:31 +0000

    added create indent n navlinks

--- estate_app/public/js/dashboard.js
+++ estate_app/public/js/dashboard.js
@@ -21,6 +21,32 @@ const chart = new frappe.Chart("#chart1", {  // or a DOM element,// new Chart() 
 
 
 
+const data3 = {
+    labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
+        "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"    ],    datasets: [
+        {            name: "Some Data", type: "line",
+            values: [25, 40, 30, 35, 8, 52, 17, -4]
+        },        {            name: "Another Set", type: "line",
+            values: [25, 50, -10, 15, 18, 32, 27, 14]
+        }    ]
+}
+
+console.log("chart added");
+const chart3 = new frappe.Chart("#chart3", {  // or a DOM element,// new Chart() in case of ES6 module with above usagetitle: "My Awesome Chart",
+    data: data,
+    height: 450,
+    type: 'pie', // or 'bar', 'line', 'scatter', 'pie', 'percentage'height: 250,
+    colors: ['#7cd6fd', '#743ee2']
+})
+
+
+
+
+
+
+
+
+// Fetching data from the and creating the chart 2 bar chart on DOM load
 
 document.addEventListener("DOMContentLoaded",async function() {
     try {
@@ -45,7 +71,7 @@ document.addEventListener("DOMContentLoaded",async function() {
             console.log(data);
             alert("Error: " + JSON.stringify(data));
         }
-        render_chart1(newdata)
+        render_chart2(newdata)
 
     } catch (error) {
         console.error("Error:", error);
@@ -53,7 +79,7 @@ document.addEventListener("DOMContentLoaded",async function() {
 });
 
 
-
+// Fetching data from the and creating the chart 2 bar chart on Button click
 
 
 document.getElementById("Generate").addEventListener("click", async function () {
@@ -79,7 +105,7 @@ document.getElementById("Generate").addEventListener("click", async function () 
             console.log(data);
             alert("Error: " + JSON.stringify(data));
         }
-        render_chart1(newdata)
+        render_chart2(newdata)
 
     } catch (error) {
         console.error("Error:", error);
@@ -92,48 +118,62 @@ document.getElementById("Generate").addEventListener("click", async function () 
 
 
 
-function render_chart1(newdata){
- console.log(newdata);
-const data2 = {
-    labels: [newdata[0].workflow_state, newdata[1].workflow_state, newdata[2].workflow_state, newdata[3].workflow_state,newdata[4].workflow_state],
-    datasets: [
-        {  name: "Indent Status", type: "bar",
-            values: [newdata[0].count, newdata[1].count, newdata[2].count, newdata[3].count,newdata[4].count]
-        }   
-    ],
-    yMarkers: [
-        {
-            label: "Threshold",
-            value: 2,
-            options: { labelPos: 'right' } // default: 'right'
-        }
-    ],
-    yRegions: [
-        {
-            label: "Optimum Value",
-            start: 0,
-            end: 5,
-            options: { labelPos: 'right' }
-        }
-    ],
-}
+// Function to render the chart 1 bar chart
 
-console.log("chart added");
-const chart2 = new frappe.Chart("#chart2", {  // or a DOM element,// new Chart() in case of ES6 module with above usagetitle: "My Awesome Chart",
-    data: data2,
-    height:400,
-    type: 'bar', // or 'bar', 'line', 'scatter', 'pie', 'percentage'height: 250,
-    colors: ['#13571d'],
-    barOptions: {
-        spaceRatio: 0.7  // Adjust the value (0 to 1), lower means thicker bars
-    },
-    axisOptions: {
-        xAxisMode: "tick",
-        xIsSeries: true,
-        dotSize: 8,
-        yDivisions:6
-      }
-})
+function render_chart2(){
+
+};
+
+
+
+
+
+
+
+// Function to render the chart 2 bar chart
+
+function render_chart2(newdata){
+        console.log(newdata);
+        const data2 = {
+            labels: [newdata[0].workflow_state, newdata[1].workflow_state, newdata[2].workflow_state, newdata[3].workflow_state,newdata[4].workflow_state],
+            datasets: [
+                {  name: "Indent Status", type: "bar",
+                    values: [newdata[0].count, newdata[1].count, newdata[2].count, newdata[3].count,newdata[4].count]
+                }   
+            ],
+            yMarkers: [
+                {
+                    label: "Threshold",
+                    value: 2,
+                    options: { labelPos: 'right' } // default: 'right'
+                }
+            ],
+            yRegions: [
+                {
+                    label: "Optimum Value",
+                    start: 0,
+                    end: 5,
+                    options: { labelPos: 'right' }
+                }
+            ],
+        }
+
+        console.log("chart added");
+        const chart2 = new frappe.Chart("#chart2", {  // or a DOM element,// new Chart() in case of ES6 module with above usagetitle: "My Awesome Chart",
+            data: data2,
+            height:400,
+            type: 'bar', // or 'bar', 'line', 'scatter', 'pie', 'percentage'height: 250,
+            colors: ['#13571d'],
+            barOptions: {
+                spaceRatio: 0.7  // Adjust the value (0 to 1), lower means thicker bars
+            },
+            axisOptions: {
+                xAxisMode: "tick",
+                xIsSeries: true,
+                dotSize: 8,
+                yDivisions:6
+            }
+        })
 
 
 };
